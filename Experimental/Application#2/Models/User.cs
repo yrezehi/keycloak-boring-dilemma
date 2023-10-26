46d4140ebf7002cec5ea6b0e8e53b693034fe3d1
unknown <yrezehi@gmail.com>
2023-10-26 08:58:15 -0700

    setup: users

---
+++ Experimental/Application#2/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Application_1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
