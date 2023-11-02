b0d59c330988da23d69db67ba2977fd7501030d8
unknown <yrezehi@gmail.com>
2023-11-01 17:36:07 -0700

    add: keycloack config

--- Experimental/Keycloack-Sample/Program.cs
+++ Experimental/Keycloack-Sample/Program.cs
@@ -1,9 +1,26 @@
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// keycloack integration
+builder.Services.AddKeycloakAuthentication(builder.Configuration.GetSection(KeycloakAuthenticationOptions.Section).Get<KeycloakAuthenticationOptions>()!);
+
+builder.Services
+    .AddAuthorization(options => options.AddPolicy("IsAdmin", builder =>
+    {
+        builder.RequireRealmRoles("admin");
+        builder.RequireResourceRoles("r-admin");
+        // TokenValidationParameters.RoleClaimType is overriden
+        // by KeycloakRolesClaimsTransformation
+        builder.RequireRole("r-admin");
+    }))
+    .AddKeycloakAuthorization(builder.Configuration.GetSection(KeycloakProtectionClientOptions.Section).Get<KeycloakProtectionClientOptions>()!);
+  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
