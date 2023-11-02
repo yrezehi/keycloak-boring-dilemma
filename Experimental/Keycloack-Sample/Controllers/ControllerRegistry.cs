a1acda2ff1f3ab8489d8b98cfab4a23e6e7cbee2
unknown <yrezehi@gmail.com>
2023-11-01 17:42:55 -0700

    add: ControllerRegistry

---
+++ Experimental/Keycloack-Sample/Controllers/ControllerRegistry.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Keycloack_Sample.Controllers
+{
+    public static class ControllerRegistry
+    {
+        public static void RegisterControllers(this WebApplication application) =>
+            application.IndexEndpoint();
+
+        private static void IndexEndpoint(this WebApplication application) =>
+           application.MapGet("/", () => Results.Ok());
+    }
+}
--- Experimental/Keycloack-Sample/Program.cs
+++ Experimental/Keycloack-Sample/Program.cs
@@ -1,5 +1,6 @@
 using Keycloak.AuthServices.Authentication;
 using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Sdk.Admin;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,7 @@ builder.Services.AddSwaggerGen();
 // keycloack integration
 builder.Services.AddKeycloakAuthentication(builder.Configuration.GetSection(KeycloakAuthenticationOptions.Section).Get<KeycloakAuthenticationOptions>()!);
 
-builder.Services
-    .AddAuthorization(options => options.AddPolicy("IsAdmin", builder =>
+builder.Services.AddAuthorization(options => options.AddPolicy("IsAdmin", builder =>
     {
         builder.RequireRealmRoles("admin");
         builder.RequireResourceRoles("r-admin");
@@ -20,7 +20,9 @@ builder.Services
         builder.RequireRole("r-admin");
     }))
     .AddKeycloakAuthorization(builder.Configuration.GetSection(KeycloakProtectionClientOptions.Section).Get<KeycloakProtectionClientOptions>()!);
-  
+
+builder.Services.AddKeycloakAdminHttpClient(builder.Configuration.GetSection(KeycloakAdminClientOptions.Section).Get<KeycloakAdminClientOptions>()!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
