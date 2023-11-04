7122483eba556cda29fdc6b9499c2f54f7410e74
unknown <yrezehi@gmail.com>
2023-11-04 09:25:34 -0700

    init: auth-mvc

--- Experimental/Keycloack-Sample-MVC/Program.cs
+++ Experimental/Keycloack-Sample-MVC/Program.cs
@@ -1,7 +1,73 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    //Sets cookie authentication scheme
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+
+            .AddCookie(cookie =>
+            {
+                //Sets the cookie name and maxage, so the cookie is invalidated.
+                cookie.Cookie.Name = "keycloak.cookie";
+                cookie.Cookie.MaxAge = TimeSpan.FromMinutes(60);
+                cookie.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+                cookie.SlidingExpiration = true;
+            })
+            .AddOpenIdConnect(options =>
+            {
+                /*
+                 * ASP.NET core uses the http://*:5000 and https://*:5001 ports for default communication with the OIDC middleware
+                 * The app requires load balancing services to work with :80 or :443
+                 * These needs to be added to the keycloak client, in order for the redirect to work.
+                 * If you however intend to use the app by itself then,
+                 * Change the ports in launchsettings.json, but beware to also change the options.CallbackPath and options.SignedOutCallbackPath!
+                 * Use LB services whenever possible, to reduce the config hazzle :)
+                */
+
+                //Use default signin scheme
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                //Keycloak server
+                options.Authority = builder.Configuration.GetSection("Keycloak")["ServerRealm"];
+                //Keycloak client ID
+                options.ClientId = builder.Configuration.GetSection("Keycloak")["ClientId"];
+                //Keycloak client secret
+                options.ClientSecret = builder.Configuration.GetSection("Keycloak")["ClientSecret"];
+                //Keycloak .wellknown config origin to fetch config
+                options.MetadataAddress = builder.Configuration.GetSection("Keycloak")["Metadata"];
+                //Require keycloak to use SSL
+                options.RequireHttpsMetadata = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                //Save the token
+                options.SaveTokens = true;
+                //Token response type, will sometimes need to be changed to IdToken, depending on config.
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                //SameSite is needed for Chrome/Firefox, as they will give http error 500 back, if not set to unspecified.
+                options.NonceCookie.SameSite = SameSiteMode.Unspecified;
+                options.CorrelationCookie.SameSite = SameSiteMode.Unspecified;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = ClaimTypes.Role,
+                    ValidateIssuer = true
+                };
+
+
+            });
 
 var app = builder.Build();
 
