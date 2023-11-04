0d20be28714a396569e0ca619d52281f8d30ebc6
unknown <yrezehi@gmail.com>
2023-11-04 08:29:17 -0700

    sync

--- Experimental/Keycloack-Sample/Controllers/ControllerRegistry.cs
+++ Experimental/Keycloack-Sample/Controllers/ControllerRegistry.cs
@@ -1,20 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 
 namespace Keycloack_Sample.Controllers
 {
     public static class ControllerRegistry
     {
-        public static void RegisterControllers(this WebApplication application)
-        {
+        public static void RegisterControllers(this WebApplication application) =>
             application.IndexEndpoint();
-            application.ClaimProperty();
-        }
 
         private static void IndexEndpoint(this WebApplication application) =>
-           application.MapGet("/", () => Results.Ok("Entry "));
-
-        private static void ClaimPropertyEndpoint(this WebApplication application) =>
-            application.MapGet("/", (ClaimsPrincipal principal) => Results.Ok(principal.Identity!.Name)).RequireAuthorization("Admin");
+            application.MapGet("/", (ClaimsPrincipal principal) =>
+                Results.Ok(principal.Identity!.Name)
+            );
     }
 }
--- Experimental/Keycloack-Sample/Program.cs
+++ Experimental/Keycloack-Sample/Program.cs
@@ -1,12 +1,41 @@
+using Keycloack_Sample.Controllers;
 using Keycloak.AuthServices.Authentication;
 using Keycloak.AuthServices.Authorization;
 using Keycloak.AuthServices.Sdk.Admin;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    // Include 'SecurityScheme' to use JWT Authentication
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+
+});
 
 // keycloack integration
 builder.Services.AddKeycloakAuthentication(builder.Configuration.GetSection(KeycloakAuthenticationOptions.Section).Get<KeycloakAuthenticationOptions>()!);
@@ -22,6 +51,11 @@ builder.Services.AddAuthorization(options => options.AddPolicy("IsAdmin", builde
 builder.Services.AddKeycloakAdminHttpClient(builder.Configuration.GetSection(KeycloakAdminClientOptions.Section).Get<KeycloakAdminClientOptions>()!);
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.RegisterControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
