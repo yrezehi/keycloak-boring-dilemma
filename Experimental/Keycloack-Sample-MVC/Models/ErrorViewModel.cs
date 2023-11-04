51f447383b2cfab1f40eafc29c9cb8985765083c
unknown <yrezehi@gmail.com>
2023-11-04 09:24:13 -0700

    init: ks-mvc

---
+++ Experimental/Keycloack-Sample-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Keycloack_Sample_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
