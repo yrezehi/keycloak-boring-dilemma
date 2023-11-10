52b33b07063ce440980df077e6ce1c6d3dc02f30
unknown <yrezehi@gmail.com>
2023-11-10 11:27:56 -0800

    clean: up sample #3

--- Experimental/Keycloack-Sample-MVC/Controllers/HomeController.cs
+++ Experimental/Keycloack-Sample-MVC/Controllers/HomeController.cs
@@ -2,97 +2,62 @@
 using Keycloack_Sample_MVC.Util;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace Keycloack_Sample_MVC.Controllers
 {
     public class HomeController : Controller
     {
-   
-
         public async Task<ActionResult> Callback()
         {
-            var accessToken = HttpContext.Request.Headers["Authorization"];
-            var refreshToken = HttpContext.Request.Headers["RefreshToken"];
-            var decodedToken = DecodeToken(accessToken);
-            var username = decodedToken.Claims.FirstOrDefault(x => x.Type == "preferred_username").Value;
-            var fullname = decodedToken.Claims.FirstOrDefault(x => x.Type == "name").Value;
-            var claims = new[]
-            {
-                new Claim("UserName",username),
-                new Claim("FullName",fullname),
-                new Claim("AccessToken",accessToken.ToString()),
-                new Claim("RefreshToken",refreshToken.ToString()),
-            };
+            string? accessToken = HttpContext.Request.Headers["Authorization"];
 
-            var identity = new ClaimsIdentity(claims, "keycloak_sso_auth");
+            if(accessToken == null)
+            {
+                return BadRequest("Authorization wasn't provided!");
+            }
+
+            var decodedToken = TokenExchangeUtil.DecodeToken(accessToken);
+
+            var usernameClaim = decodedToken.Claims.FirstOrDefault(x => x.Type == "preferred_username");
+
+            if (usernameClaim == null)
+            {
+                return BadRequest("Username wasn't provided!");
+            }
+
+            var nameClaim = decodedToken.Claims.FirstOrDefault(x => x.Type == "name");
+
+            if (nameClaim == null)
+            {
+                return BadRequest("Name wasn't provided!");
+            }
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim("UserName",usernameClaim.Value),
+                new Claim("Name", nameClaim.Value),
+                new Claim("AccessToken",accessToken.ToString()),
+            }, "keycloak_sso_auth");
 
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), new AuthenticationProperties { });
+            
             return RedirectToAction("Index");
         }
 
         [Authorize]
-        public async Task<IActionResult> Index()
-        {
-            await HttpContext.SignOutAsync();
-            //Find claims for the current user
-            ClaimsPrincipal currentUser = this.User;
-            //Get username, for keycloak you need to regex this to get the clean username
-            var currentUserName = currentUser.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            //logs an error so it's easier to find - thanks debug.
-            _logger.LogError(currentUserName);
-
-            /*
-             * Token exchange implementation
-             * Uncomment section below
-             */
-            
-            //Call a token exchange to call another service in keycloak
-            //Remember to implement a logger with the default constructor for more visibility
-            TokenExchangeUtil exchange = new TokenExchangeUtil();
-            //Do a refresh token, if the service you need to call has a short lived token time
-            var newAccessToken = await exchange.GetTokenAsync();
-            //Use the access token to call the service that exchanged the token
-            //Example:
-            // MyService myService = new MyService/();
-            //var myService = await myService.GetDataAboutSomethingAsync(serviceAccessToken):
-
-            var decodedToken = DecodeToken(newAccessToken);
-            var username = decodedToken.Claims.FirstOrDefault(x => x.Type == "preferred_username").Value;
-            var claims = new[]
-            {
-                new Claim("UserName",username),
-                new Claim("AccessToken",newAccessToken.ToString()),
-            };
-
-            var identity = new ClaimsIdentity(claims, "keycloak_sso_auth");
-
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), new AuthenticationProperties { });
-
-            //Get all claims for roles that you have been granted access to 
-            IEnumerable<Claim> roleClaims = User.FindAll(ClaimTypes.Role);
-            IEnumerable<string> roles = roleClaims.Select(r => r.Value);
-
-            //Another way to display all role claims
-            var currentClaims = currentUser.FindAll(ClaimTypes.Role).ToList();
-         
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index() =>
+            View();
+ 
+        public IActionResult Privacy() =>
+            View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      
     }
 }
--- Experimental/Keycloack-Sample-MVC/Util/TokenExchangeUtil.cs
+++ Experimental/Keycloack-Sample-MVC/Util/TokenExchangeUtil.cs
@@ -9,17 +9,30 @@ namespace Keycloack_Sample_MVC.Util
         public readonly HttpClient HttpClient;
         public readonly string ServerURL;
         public readonly string ClientId;
+        public readonly string ClientSecert;
+
+        private static string DEFAULT_GRANT_TYPE = "password";
 
         public TokenExchangeUtil() =>        
-            (HttpClient, ServerURL, ClientId) 
+            (
+                HttpClient,
+                ServerURL,
+                ClientId,
+                ClientSecert
+            ) 
                 =
-            (new HttpClient(), ConfigurationUtil.GetValue<string>("Keycloak:TokenExchange"), ConfigurationUtil.GetValue<string>("Keycloak:ClientId"));
+            (
+                new HttpClient(),
+                ConfigurationUtil.GetValue<string>("Keycloak:TokenExchange"),
+                ConfigurationUtil.GetValue<string>("Keycloak:ClientId"),
+                ConfigurationUtil.GetValue<string>("Keycloak:ClientSecret")
+            );
 
         public async Task<string> GetTokenAsync()
         {
             HttpResponseMessage tokenResponse = await HttpClient.PostAsync(
                 ServerURL,
-                new FormUrlEncodedContent(BuildAuthenticationForm())
+                new FormUrlEncodedContent(BuildAuthenticationForm("test@test.com", "password"))
             );
 
             var jsonContent = await tokenResponse.Content.ReadAsStringAsync();
@@ -37,13 +50,13 @@ namespace Keycloack_Sample_MVC.Util
         public static JwtSecurityToken DecodeToken(string token) =>
             new JwtSecurityTokenHandler().ReadToken(token) as JwtSecurityToken ?? throw new ArgumentException("Not valid token!");
 
-        private Dictionary<string, string> BuildAuthenticationForm() => new()
+        private Dictionary<string, string> BuildAuthenticationForm(string username, string password) => new()
         {
-            {"grant_type", "password"},
+            {"grant_type", DEFAULT_GRANT_TYPE},
             {"client_id", ClientId },
-            {"username", "test@test.com"},
-            {"password", "password"},
-            {"client_secret", "Tgx4lvbyhho7oNFmiIupDRVA8ioQY7PW"}
+            {"client_secret", ClientSecert},
+            {"username", username},
+            {"password", password},
         };
     }
 }
